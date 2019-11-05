01ddb0649fee7edabd1aac0dd6f09cefff81103a
Sanjay Kumar <sanjay13.kumar@paytmmall.com>
2019-11-05 17:05:07 +0530

    refactor combined normaliser

--- src/main/java/com/paytmmall/spellchecker/dictionary/normaliser/impl/CombinedNormaliser.java
+++ src/main/java/com/paytmmall/spellchecker/dictionary/normaliser/impl/CombinedNormaliser.java
@@ -13,8 +13,7 @@ import java.util.concurrent.ExecutionException;
 
 @Service
 public class CombinedNormaliser implements Normaliser {
-    private static final Logger logger = LoggerFactory.getLogger(CombinedNormaliser.class);
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(CombinedNormaliser.class);
 
     @Autowired
     private CatalogTokensNormaliser catalogTokensNormaliser;
@@ -27,33 +26,12 @@ public class CombinedNormaliser implements Normaliser {
 
     @Override
     public void normalise() throws IOException {
-        CompletableFuture<Void> future1
-                = CompletableFuture.runAsync(() -> {
-            try {
-                catalogTokensNormaliser.normalise();
-            } catch (IOException e) {
-                logger.error("Error during Catalog Token file read ", e);
-                throw new CustomExceptions.FileReadException(e.getMessage());
-            }
-        });
-        CompletableFuture<Void> future2
-                = CompletableFuture.runAsync(() -> {
-            try {
-                englishDictionaryNormaliser.normalise();
-            } catch (IOException e) {
-                logger.error("Error during English Dictionary file read ", e);
-                throw new CustomExceptions.FileReadException(e.getMessage());
-            }
-        });
-        CompletableFuture<Void> future3
-                = CompletableFuture.runAsync(() -> {
-            try {
-                userQueryTokenNormaliser.normalise();
-            } catch (IOException e) {
-                logger.error("Error during User Query Token file read ", e);
-                throw new CustomExceptions.FileReadException(e.getMessage());
-            }
-        });
+        CompletableFuture<Void> future1 = getCompletableFutureForNormaliser(catalogTokensNormaliser,
+                "Catalog Tokens");
+        CompletableFuture<Void> future2 = getCompletableFutureForNormaliser(englishDictionaryNormaliser,
+                "English Dictionary");
+        CompletableFuture<Void> future3 = getCompletableFutureForNormaliser(userQueryTokenNormaliser,
+                "User query tokens");
 
         CompletableFuture<Void> combinedFuture
                 = CompletableFuture.allOf(future1, future2, future3);
@@ -61,9 +39,20 @@ public class CombinedNormaliser implements Normaliser {
         try {
             combinedFuture.get(); // wait for all three to complete
         } catch (InterruptedException | ExecutionException e) {
-            logger.error("Error occured during normalisation of token files ", e);
+            LOGGER.error("Error occurred during normalisation of token files ", e);
             throw new CustomExceptions.FileReadException(e.getMessage());
         }
+    }
+
+    private CompletableFuture<Void> getCompletableFutureForNormaliser(Normaliser normaliser, String source) {
+        return CompletableFuture.runAsync(() -> {
+            try {
+                normaliser.normalise();
+            } catch (IOException e) {
+                LOGGER.error("Error during {} file read. Exception: {}", source, e);
+                throw new CustomExceptions.FileReadException(e.getMessage());
+            }
+        });
     }
 
 }
